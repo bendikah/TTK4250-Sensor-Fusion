00fdac1af4b3bace59be98cb170a9ff3113b6c39
bendikah <bendikah@stud.ntnu.no>
2020-10-17 19:18:36 +0200

    first try at task 1 graded E 2

---
+++ Sensor Fusion Graded Assignment 2/gradedINS/utils.py
@@ -0,0 +1,20 @@
+import numpy as np
+from mytypes import ArrayLike
+
+
+def cross_product_matrix(n: ArrayLike, debug: bool = True) -> np.ndarray:
+    assert len(n) == 3, f"utils.cross_product_matrix: Vector not of length 3: {n}"
+    vector = np.array(n, dtype=float).reshape(3)
+
+    S = np.array([[0, -n[2], n[1]],[n[2], 0, -n[0]], [-n[1], n[0], 0]]) #cross product matrix eq. 10.5
+    
+    if debug:
+        assert S.shape == (
+            3,
+            3,
+        ), f"utils.cross_product_matrix: Result is not a 3x3 matrix: {S}, \n{S.shape}"
+        assert np.allclose(
+            S.T, -S
+        ), f"utils.cross_product_matrix: Result is not skew-symmetric: {S}"
+
+    return S
